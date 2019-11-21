92e3f8d7a2ab1d2e49b7109f735d88bf53ec7923
Chris Patrick <atomize101@gmail.com>
2019-11-21 17:02:10 -0500

    Adding buttons and Selectors

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ class Timer {
     start() {
         console.log('Time to start')
     }
-}
+};
 
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
