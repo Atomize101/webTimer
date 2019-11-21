97ce3f41f0fb20c738c4fd9549ddbb41480a1fc1
Chris Patrick <atomize101@gmail.com>
2019-11-21 16:51:09 -0500

    Creating Timer Class

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hi there')
+class Timer {
+    constructor(durationInput, startButton, pauseButton) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+
+        this.startButton.addEventListener('click', this.start);
+    }
+
+    start() {
+        console.log('Time to start')
+    }
+}
+
