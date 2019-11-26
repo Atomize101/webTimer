c805734c8db47f699ed7a54fe31922b4942e9c76
Chris Patrick <atomize101@gmail.com>
2019-11-26 11:11:15 -0500

    Modifing timer method

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if(callbacks) {
+            this.onStart = callbacks.onStart;
+        }
 
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
     }
 
     start = () => {
+        if(this.onStart) {
+            this.onStart();
+        }
         this.tick();
         this.interval = setInterval(this.tick, 1000);
     };
 
     tick = () => {
-        this.timeRemaining = this.timeRemaining - 1;
+        if(this.timeRemaining <= 0) {
+            this.pause();
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;    
+        }
     }
     pause = () => {
         clearInterval(this.interval);
@@ -33,5 +43,15 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+   onStart() {
+       console.log('timer started')
+   },
+   onTick() {
+
+   },
+   onComplete() {
+
+   }
+});
 
